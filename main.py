40e796d78436862a3a17884cce24c947966bc602
Gesner Deslandes <deslandes78@gmail.com>
2026-03-15 09:03:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-import psycopg2
-from dotenv import load_dotenv
-import os
-# Load environment variables from .env
-load_dotenv()
-# Fetch variables
-DATABASE_URL = os.getenv("DATABASE_URL")
-# Connect to the database
-connection = psycopg2.connect(DATABASE_URL)
-db.oyyywfwufogknghzyeov.supabase.co
+
